d31b038f8eff0d22252e32c6cc6a03f60a6ccb54
Adam Patridge <patridge@gmail.com>
2018-02-16 17:18:45 -0700

    Ex1: update with runtime permissions.

--- Exercise 1/Start/DroidMapping/MainActivity.cs
+++ Exercise 1/Start/DroidMapping/MainActivity.cs
@@ -1,41 +1,56 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content.PM;
 using Android.Gms.Maps;
+using System.Threading.Tasks;
 using Android.Gms.Maps.Model;
 
 namespace DroidMapping
 {
-    [Activity(Label = "DroidMapping", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "DroidMapping", MainLauncher = true)]
     public class MainActivity : Activity, IOnMapReadyCallback
     {
-        GoogleMap map;
+        MappingPermissionsHelper permissionHelper;
         MapFragment mapFragment;
+        GoogleMap map;
+        Task<bool> getLocationPermissionsAsync;
 
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
+
+            permissionHelper = new MappingPermissionsHelper(this);
+            getLocationPermissionsAsync = permissionHelper.CheckAndRequestPermissions();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Find and load the map fragment
             mapFragment = FragmentManager.FindFragmentById(Resource.Id.map) as MapFragment;
+
             mapFragment.GetMapAsync(this);
         }
 
-        public void OnMapReady(GoogleMap googleMap)
+        public async void OnMapReady(GoogleMap googleMap)
         {
             map = googleMap;
 
             map.MapType = GoogleMap.MapTypeHybrid;
             map.UiSettings.ZoomControlsEnabled = true;
 
+            // Make sure we have user permission for location use before we call any methods requiring permission.
+            await getLocationPermissionsAsync;
             // Center on Xamarin HQ
             LatLng coord = new LatLng(37.797679, -122.401816);
             CameraUpdate update = CameraUpdateFactory.NewLatLngZoom(coord, 17);
             map.AnimateCamera(update);
         }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            permissionHelper.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
-
