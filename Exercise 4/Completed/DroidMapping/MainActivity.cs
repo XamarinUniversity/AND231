ddb1eaa7c56a3ef735fcafee8e8e78c29de33891
Adam Patridge <patridge@gmail.com>
2018-02-21 12:44:06 -0700

    Ex4: fixes and permission updates.

--- Exercise 4/Completed/DroidMapping/MainActivity.cs
+++ Exercise 4/Completed/DroidMapping/MainActivity.cs
@@ -1,47 +1,56 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content.PM;
 using Android.Gms.Maps;
+using System.Threading.Tasks;
 using Android.Gms.Maps.Model;
 using Android.Locations;
+using Android.Runtime;
 using Android.Gms.Common.Apis;
 using Android.Gms.Location;
 using Android.Gms.Common;
+using Android.Gms.Plus;
 
 namespace DroidMapping
 {
-    [Activity(Label = "DroidMapping", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity, IOnMapReadyCallback, 
-                Android.Locations.ILocationListener,
-                Android.Gms.Location.ILocationListener,
-				GoogleApiClient.IConnectionCallbacks,
-				GoogleApiClient.IOnConnectionFailedListener
+    [Activity(Label = "DroidMapping", MainLauncher = true)]
+    public class MainActivity : Activity, IOnMapReadyCallback,
+        Android.Locations.ILocationListener,
+        Android.Gms.Location.ILocationListener,
+        GoogleApiClient.IConnectionCallbacks,
+        GoogleApiClient.IOnConnectionFailedListener
     {
-        GoogleMap map;
+        MappingPermissionsHelper permissionHelper;
         MapFragment mapFragment;
+        GoogleMap map;
+        Task<bool> getLocationPermissionsAsync;
+        Marker currentLocationMarker;
         GoogleApiClient apiClient;
 
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
+
+            permissionHelper = new MappingPermissionsHelper(this);
+            getLocationPermissionsAsync = permissionHelper.CheckAndRequestPermissions();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Find and load the map fragment
             mapFragment = FragmentManager.FindFragmentById(Resource.Id.map) as MapFragment;
+
             mapFragment.GetMapAsync(this);
 
-            if (GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this) == 0) 
-			{
+            if (GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this) == 0)
+            {
                 apiClient = new GoogleApiClient.Builder(this)
+                    .AddConnectionCallbacks(this)
+                    .AddOnConnectionFailedListener(this)
                     .AddApi(LocationServices.API)
-					.AddOnConnectionFailedListener(this)
-					.AddConnectionCallbacks(this)
-// Uncomment to test the error recovery code.
-//                    .AddApi(PlusClass.API)
-//                    .AddScope(PlusClass.ScopePlusLogin)
+                    // Uncomment to test the error recovery code.
+                    //.AddApi(PlusClass.API)
+                    //.AddScope(PlusClass.ScopePlusLogin)
                     .Build();
             }
         }
@@ -49,91 +58,47 @@ namespace DroidMapping
         protected override void OnStart()
         {
             base.OnStart();
-            if (apiClient != null && map != null) {
+            if (apiClient != null && map != null)
+            {
                 apiClient.Connect();
             }
         }
-
         protected override void OnStop()
         {
             base.OnStop();
-            if (apiClient != null) {
+            if (apiClient != null)
+            {
                 apiClient.Disconnect();
             }
         }
 
-        public void OnMapReady(GoogleMap googleMap)
+        public async void OnMapReady(GoogleMap googleMap)
         {
             map = googleMap;
 
             map.MapType = GoogleMap.MapTypeHybrid;
             map.UiSettings.ZoomControlsEnabled = true;
 
-            Criteria criteria = new Criteria {
+            // Make sure we have user permission for location use before we call any methods requiring permission.
+            await getLocationPermissionsAsync;
+
+            Criteria criteria = new Criteria
+            {
                 Accuracy = Accuracy.Fine,
                 PowerRequirement = Power.NoRequirement,
             };
 
-            if (apiClient != null) {
+            if (apiClient != null)
+            {
                 if (!apiClient.IsConnected)
                     apiClient.Connect();
             }
-            else {
+            else
+            {
                 LocationManager locManager = LocationManager.FromContext(this);
                 locManager.RequestLocationUpdates(5000, 100f, criteria, this, null);
             }
         }
-
-        public void OnConnected(Bundle connectionHint)
-        {
-            LocationRequest locationRequest = LocationRequest.Create()
-                .SetPriority(LocationRequest.PriorityHighAccuracy)
-                .SetInterval(5000)
-                .SetSmallestDisplacement(100f);
-            
-            LocationServices.FusedLocationApi.RequestLocationUpdates(
-                apiClient, locationRequest, this);
-        } 
-
-        public void OnConnectionSuspended(int cause)
-        {
-        }
-
-        bool resolvingError;
-        const int ResolvePlayErrorRequestId = 1001;
-
-        public void OnConnectionFailed(ConnectionResult result)
-        {
-            if (result.HasResolution) {
-                if (!resolvingError) {
-                    resolvingError = true;
-                    try {
-                    result.StartResolutionForResult(this, 
-                        ResolvePlayErrorRequestId);
-                    }
-                    catch (Android.Content.IntentSender.SendIntentException) {
-                        // Canceled - restart connection
-                        resolvingError = false;
-                        apiClient.Connect();
-                    }
-                }
-            }
-        }
-
-        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
-        {
-            if (requestCode == ResolvePlayErrorRequestId) {
-                resolvingError = false;
-                if (resultCode == 0) {
-                    if (!apiClient.IsConnecting 
-                        && !apiClient.IsConnected) {
-                        apiClient.Connect();
-                    }
-                }
-            }
-        }
-
-        Marker currentLocationMarker;
 
         public void OnLocationChanged(Location location)
         {
@@ -141,10 +106,12 @@ namespace DroidMapping
             CameraUpdate update = CameraUpdateFactory.NewLatLngZoom(coord, 17);
             map.AnimateCamera(update);
 
-            if (currentLocationMarker != null) {
+            if (currentLocationMarker != null)
+            {
                 currentLocationMarker.Position = coord;
             }
-            else {
+            else
+            {
                 var markerOptions = new MarkerOptions()
                     .SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueBlue))
                     .SetPosition(coord)
@@ -162,9 +129,66 @@ namespace DroidMapping
         {
         }
 
-        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
         {
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            permissionHelper.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnConnected(Bundle connectionHint)
+        {
+            LocationRequest locationRequest = LocationRequest.Create()
+                .SetPriority(LocationRequest.PriorityHighAccuracy)
+                .SetInterval(5000)
+                .SetSmallestDisplacement(100f);
+
+            LocationServices.FusedLocationApi.RequestLocationUpdates(apiClient, locationRequest, this);
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+        }
+
+        bool resolvingError;
+        const int ResolvePlayErrorRequestId = 1001;
+
+        public void OnConnectionFailed(ConnectionResult result)
+        {
+            if (!resolvingError && result.HasResolution)
+            {
+                resolvingError = true;
+                try
+                {
+                    result.StartResolutionForResult(this,
+                        ResolvePlayErrorRequestId);
+                }
+                catch (Android.Content.IntentSender.SendIntentException)
+                {
+                    // Canceled - restart connection
+                    resolvingError = false;
+                    apiClient.Connect();
+                }
+            }
+        }
+        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
+        {
+            if (requestCode == ResolvePlayErrorRequestId)
+            {
+                resolvingError = false;
+                if (resultCode == Android.App.Result.Canceled)
+                {
+                    if (!apiClient.IsConnecting
+                        && !apiClient.IsConnected)
+                    {
+                        apiClient.Connect();
+                    }
+                }
+            }
         }
     }
 }
-
