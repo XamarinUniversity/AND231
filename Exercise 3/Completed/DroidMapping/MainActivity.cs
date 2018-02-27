d07311489375590307fb2afc3a700c1c0dbba726
Adam Patridge <patridge@gmail.com>
2018-02-27 09:22:19 -0700

    Add runtime permissions and several other fixes. (#1)
    * Ex1: update with runtime permissions.
    
    * Ex2: update with runtime permissions.
    
    * Ex2: add missing helper (and csproj edit).
    
    * [AND231] Ex3: fixes and permission updates.
    
    * Ex4: fixes and permission updates.
    
    * Ex4: updated manifest and help comments for login error handling.
    
    * Ex4: match up whitespace to later exercises.
    
    * Ex5: fixes and permission updates.
    
    * Ex6: fixes and permission updates.
    
    Also revised use of Geocoder for updated API intent.
    
    * Unify whitespace in helper class.
    
    * Add missing helper to Ex1. Remove unused class everywhere.

--- Exercise 3/Completed/DroidMapping/MainActivity.cs
+++ Exercise 3/Completed/DroidMapping/MainActivity.cs
@@ -1,38 +1,51 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content.PM;
 using Android.Gms.Maps;
+using System.Threading.Tasks;
 using Android.Gms.Maps.Model;
 using Android.Locations;
+using Android.Runtime;
 
 namespace DroidMapping
 {
-    [Activity(Label = "DroidMapping", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "DroidMapping", MainLauncher = true)]
     public class MainActivity : Activity, IOnMapReadyCallback, ILocationListener
     {
-        GoogleMap map;
+        MappingPermissionsHelper permissionHelper;
         MapFragment mapFragment;
+        GoogleMap map;
+        Task<bool> getLocationPermissionsAsync;
+        Marker currentLocationMarker;
 
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
+
+            permissionHelper = new MappingPermissionsHelper(this);
+            getLocationPermissionsAsync = permissionHelper.CheckAndRequestPermissions();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Find and load the map fragment
             mapFragment = FragmentManager.FindFragmentById(Resource.Id.map) as MapFragment;
+
             mapFragment.GetMapAsync(this);
         }
 
-        public void OnMapReady(GoogleMap googleMap)
+        public async void OnMapReady(GoogleMap googleMap)
         {
             map = googleMap;
 
             map.MapType = GoogleMap.MapTypeHybrid;
             map.UiSettings.ZoomControlsEnabled = true;
 
-            Criteria criteria = new Criteria {
+            // Make sure we have user permission for location use before we call any methods requiring permission.
+            await getLocationPermissionsAsync;
+
+            Criteria criteria = new Criteria
+            {
                 Accuracy = Accuracy.Fine,
                 PowerRequirement = Power.NoRequirement,
             };
@@ -41,18 +54,18 @@ namespace DroidMapping
             locManager.RequestLocationUpdates(5000, 100f, criteria, this, null);
         }
 
-        Marker currentLocationMarker;
-
         public void OnLocationChanged(Location location)
         {
             LatLng coord = new LatLng(location.Latitude, location.Longitude);
             CameraUpdate update = CameraUpdateFactory.NewLatLngZoom(coord, 17);
             map.AnimateCamera(update);
 
-            if (currentLocationMarker != null) {
+            if (currentLocationMarker != null)
+            {
                 currentLocationMarker.Position = coord;
             }
-            else {
+            else
+            {
                 var markerOptions = new MarkerOptions()
                     .SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueBlue))
                     .SetPosition(coord)
@@ -70,9 +83,15 @@ namespace DroidMapping
         {
         }
 
-        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
         {
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            permissionHelper.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
-
